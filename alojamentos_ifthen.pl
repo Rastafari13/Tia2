fa02f0bb6a9a473b4d647bc0dba133d0a5a1f71d
Rastafari13 <mscancela2001@hotmail.com>
2022-05-02 12:07:02 +0100

    Trabalho final Tia2

--- alojamentos_ifthen.pl
+++ alojamentos_ifthen.pl
@@ -1,4 +1,26 @@
-%:- dynamic (<==)/2.
+:- dynamic (<==)/2.
 
-%nao<==[[idade=idade36a45], [idade=idade46a55], [tipo=hamburguers], [aspeto=variedade, idade=idade18a25]].
-%sim<==[[aspeto=comida], [aspeto=atendimento], [preco=precoacima20], [idade=idade15a17], [tipo=francesinha], [preco=preco5a7, tipo=carne]].
+%sim comer no alojamento, nao - comer fora
+
+nao<==[[idade=idade26a33], [idade=idade18a25, lazer=sim],[ativa=sim],[idade=idade34a45, ativa=sim]].
+sim<==[[idade=idademaior45], [idade=idade34a45,ativa=nao]].
+
+
+%if Qual a sua idade? = 26 a 33 then Refeições em restaurantes locais  (1 / 13)
+%if Qual a sua idade? = 18 a 25 and No local em que ficasse alojado, gostaria que lhe fossem proporcionados momentos de lazer (piscina ou spa)? = Sim then Refeições em restaurantes locais  (6 / 54)
+%else Refeições no alojamento  (45 / 16)
+
+%if Qual a sua idade? = 26 a 33 then Refeições em restaurantes locais  (1 / 21)
+%if Em ambiente de férias, gosta de ser uma pessoa ativa? = Sim then Refeições em restaurantes locais  (16 / 80)
+%else Refeições no alojamento  (32 / 3)
+
+%Restaurantes locais:
+%- idade 26 a 33
+%- idade 18 a 25 and querer momentos de lazer
+%- ser pessoa ativa
+%- idade 34 a 45 and ser pessoa ativa
+
+%Restaurante no alojamento:
+%- todos os outros
+%- idade maior 45
+%- idade 34 a 45 and nao ser pessoa ativa
--- interface.pl
+++ interface.pl
@@ -1,30 +1,31 @@
 
-:-[satisfy, induce_ifthen, atributos, alojamentos_ifthen].
+:-[satisfy, induce_ifthen, alojamentos_ifthen].
 
 % the goal is to classify
-%learn_rules:-
-%	learn(nao),  % negative examples
-%	learn(sim), % positive examples
-%        % save new rules:
-%	tell('restaurantes_ifthen.pl'),
-%	listing(<==),
-%	told.
+learn_rules:-
+	learn(nao),  % negative examples
+	learn(sim), % positive examples
+        % save new rules:
+	tell('alojamentos_ifthen.pl'),
+	listing(<==),
+	told.
 
 % example of classifying
-q1(Class):- classify([idade=idade18a25,motivo=ilha,restaurante=sim,pa=sim,ginasio=sim],Class), write(classify([idade=idade18a25,motivo=ilha,restaurante=sim,pa=sim,ginasio=sim],Class)).
-q2(Class):- classify([idade=idade18a25,motivo=ilha,restaurante=nao,pa=sim,ginasio=sim],Class).
-q3(Class):- classify([idade=idade18a25,motivo=ilha,restaurante=nao,pa=sim,ginasio=nao],Class).
-q5(Class):- classify([idade=idade26a33,motivo=ilha,restaurante=sim,pa=sim,ginasio=sim], Class).
+q1(Class):- classify([idade=idade18a25,lazer=sim,ativa=sim],Class).
+q2(Class):- classify([idade=idade34a45,ativa=nao],Class).
+%q3(Class):- classify([idade=idade18a25,motivo=ilha,restaurante=nao,pa=sim,ginasio=nao],Class).
+%q5(Class):- classify([idade=idade26a33,motivo=ilha,restaurante=sim,pa=sim,ginasio=sim], Class).
 
 q4(Class):- findall(A,fact(A),Z), classify(Z,Class), retractall(fact(_)).
 
 
 
-menu:-
+menu1:-
      write(
 "
-                  Tem duvidas entre comprar uma refeicao num restaurante ou num hipermercado?
-                  Seja bem-vindo ao sistema de recomendacao! Esperamos corresponder as suas expetativas *_*
+                  Tem duvidas se devera escolher um alojamento com restaurante onde possa fazer as suas refeicoes ou explorar restaurantes locais?
+                  Temos o sistema perfeito para si! Com base nas suas respostas, seremos a ajuda que necessita!
+
 
 "
 ),nl,
@@ -32,7 +33,7 @@ menu:-
     write(
 "____________________________________________________________________________________________________
 
-                    Indique a sua idade
+                    Qual a sua idade?
 
                     1- 18 a 25
                     2- 26 a 33
@@ -49,86 +50,70 @@ ________________________________________________________________________________
         (Idade == 1), assert(fact(idade=idade18a25));
         (Idade == 2), assert(fact(idade=idade26a33));
         (Idade == 3), assert(fact(idade=idade34a45));
-        (Idade == 4), assert(fact(idade=idademaior45)), nl,
+        (Idade == 4), assert(fact(idade=idademaior45))), nl,
 
 
     write(
 "____________________________________________________________________________________________________
 
-                    Qual o principal motivo que tem em consideracao para visitar o Funchal?
+                    Em ambiente de ferias, gosta de ser uma pessoa ativa?
 
-                    1- Conhecer a ilha
-                    2- Relaxar
+                    1- Sim
+                    2- Nao
 
                     0- Sair
 
 
 ____________________________________________________________________________________________________"
 ),nl,nl,
-    read(Motivo),
-        ((Motivo == 0), halt;
-        (Motivo == 1), assert(fact(motivo=ilha));
-        (Motivo == 2), assert(fact(motivo=relaxar)), nl,
+    read(Ativa),
+        ((Ativa == 0), halt;
+        (Ativa == 1), assert(fact(ativa=sim));
+        (Ativa == 2), assert(fact(ativa=nao))), nl,
 
     write(
 "____________________________________________________________________________________________________
 
-                    Pretende ter restaurante no alojamento?
+                    Gostaria de conhecer mais sobre a cultura do seu destino de ferias?
 
                     1- Sim
-                    2- Não
+                    2- Nao
 
                     0- Sair
 
 
 ____________________________________________________________________________________________________"
 ),nl,nl,
-    read(Restaurante),
-        ((Restaurante == 0), halt;
-        (Restaurante == 1), assert(fact(restaurante=sim));
-        (Restaurante == 2), assert(fact(restaurante=nao)), nl,
+    read(Cultura),
+        ((Cultura == 0), halt;
+        (Cultura == 1), assert(fact(conhecer=sim));
+        (Cultura == 2), assert(fact(conhecer=nao))), nl,
 
     write(
 "____________________________________________________________________________________________________
 
-                    Pretende ter o pequeno-almoço incluído na estadia ?
+                    No local em que ficasse alojado, gostaria que lhe fossem proporcionados momentos de lazer (piscina ou spa)?
 
                     1- Sim
-                    2- Não
+                    2- Nao
 
                     0- Sair
 
 
 ____________________________________________________________________________________________________"
 ),nl,nl,
-    read(Pa),
-        ((Pa == 0), halt;
-        (Pa == 1), assert(fact(pa=sim));
-        (Pa == 2), assert(fact(pa=nao)), nl,
-
-write("____________________________________________________________________________________________________
-                Pretende ter ginásio no alojamento?
-
-                    1- Sim
-                    2- Não
-
-                    0- Sair
+    read(Lazer),
+        ((Lazer == 0), halt;
+        (Lazer == 1), assert(fact(lazer=sim));
+        (Lazer == 2), assert(fact(lazer=nao))), nl,
 
 
-____________________________________________________________________________________________________"
-),nl,nl
-    read(Ginasio),
-        ((Ginasio == 0), halt;
-        (Ginasio == 1), assert(fact(ginasio=sim));
-        (Ginasio == 2), assert(fact(ginasio=nao)), nl,
-                                                                                                     "
-                                                                                                     ).
  write('RESULTADO'),nl,
 
 q4(C),
 
- write('Alojamento: '), write(C),nl,nl,
+write('Fazer as refeicoes no alojamento: '), write(C),nl,nl,
 
 
 
- write("Disfrute da sua refeicao! Obrigada!"),nl.
+write('Esperamos que tenhamos ajudado! Obrigada!'),nl.
